a4f1071e09501cbb6b3928d50425a53486344ae5
CLYP0103 <47965402+CLYP0103@users.noreply.github.com>
2019-10-04 18:13:57 +0800

    武器特效

--- Demo/Assets/Script/Controller/WeaponController.cs
+++ Demo/Assets/Script/Controller/WeaponController.cs
@@ -8,13 +8,25 @@ public class WeaponController : MonoBehaviour
     public GameObject m_Sowrd;
     public GameObject m_Gun;
     public GameObject m_Arrow;
+
+    public GameObject m_NowWeapon;
     public GameObject m_Effect;
+
+    public Dictionary<int, GameObject> m_WeaponDict;                        // 武器字典
 
     private void Awake()
     {
+
+        m_WeaponDict = new Dictionary<int, GameObject>();
         m_Weapon = transform.Find("Weapon").gameObject;
         m_Sowrd = m_Weapon.transform.Find("Sword").gameObject;
+        m_Gun = m_Weapon.transform.Find("Gun").gameObject;
         m_Effect = m_Sowrd.transform.Find("Effect").gameObject;
+        m_NowWeapon = m_Sowrd;
+
+        m_WeaponDict.Add(0, m_Sowrd);
+        m_WeaponDict.Add(1, m_Gun);
+        m_WeaponDict.Add(2, m_Arrow);
     }
 
     // Start is called before the first frame update
@@ -26,18 +38,25 @@ public class WeaponController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // 激活或关闭武器
     public void SetWeaponActive(bool flag)
     {
         m_Weapon.SetActive(flag);
+        m_NowWeapon.SetActive(flag);
     }
 
     // 特效激活
     public void SetEffectActive(bool flag)
     {
         m_Effect.SetActive(flag);
+    }
+
+    public void ChangeWeapon(int id)
+    {
+        m_NowWeapon = m_WeaponDict[id];
+        m_Effect = m_NowWeapon.transform.Find("Effect").gameObject;
     }
 }
---
+++ Demo/Assets/Script/Manager/ObjectPoolManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    private static ObjectPoolManager m_Instance;
+
+    public static ObjectPoolManager Instacne
+    {
+        get
+        {
+            return m_Instance;
+        }
+    }
+
+    private void Awake()
+    {
+        m_Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
