68a6b3ae1856b006b74d649fd5bc8fb277672ee1
CLYP0103 <47965402+CLYP0103@users.noreply.github.com>
2019-10-04 20:38:10 +0800

    特效包的导入

---
+++ Demo/Assets/Script/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject impactParticle;
+    public GameObject projectileParticle;
+    public GameObject[] trailParticles;
+
+    [HideInInspector]
+    public Vector3 impactNormal; //Used to rotate impactparticle.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * Time.deltaTime * 8.0f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
+        Destroy(this);
+    }
+}
