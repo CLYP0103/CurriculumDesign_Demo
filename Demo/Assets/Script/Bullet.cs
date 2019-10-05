27b0da07b5fb9b2a355c1f9189afb2fe49d62972
CLYP0103 <47965402+CLYP0103@users.noreply.github.com>
2019-10-05 15:39:52 +0800

    蓝色子弹和对象池连接完成
    子弹和开枪动画 未衔接匹配

--- Demo/Assets/Script/Bullet.cs
+++ Demo/Assets/Script/Bullet.cs
@@ -6,10 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public GameObject impactParticle;
     public GameObject projectileParticle;
-    public GameObject[] trailParticles;
+
+    public float speed = 8.0f;
+    public float liveTime = 10.0f;
 
     [HideInInspector]
     public Vector3 impactNormal; //Used to rotate impactparticle.
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,16 +20,24 @@ public class Bullet : MonoBehaviour
         
     }
 
+    private void OnEnable()
+    {
+        ObjectPoolManager.Instacne.RecycleGameObject("Bullet_Blue", transform.gameObject, liveTime);
+    }
+
     // Update is called once per frame
     void Update()
     {
-
-        transform.Translate(Vector3.forward * Time.deltaTime * 8.0f);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
-        Destroy(this);
+        //impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
+        impactParticle = ObjectPoolManager.Instacne.GetGameObject("Bullet_Explosion_Blue");
+        impactParticle.transform.parent = null;
+        impactParticle.transform.position = transform.position;
+        ObjectPoolManager.Instacne.RecycleGameObject("Bullet_Blue", transform.gameObject);
+        ObjectPoolManager.Instacne.RecycleGameObject("Bullet_Explosion_Blue", impactParticle,0.5f);  
     }
 }
--- Demo/Assets/Script/Manager/ObjectPoolManager.cs
+++ Demo/Assets/Script/Manager/ObjectPoolManager.cs
@@ -8,6 +8,8 @@ public class ObjectPoolManager : MonoBehaviour
     private Dictionary<string, string> m_PrefabPathDict;
     private Dictionary<string, List<GameObject>> m_PrefabDict;
 
+    public List<GameObject> g_list;
+
 
     public static ObjectPoolManager Instacne
     {
@@ -23,24 +25,42 @@ public class ObjectPoolManager : MonoBehaviour
         m_PrefabPathDict = new Dictionary<string, string>();
         m_PrefabDict = new Dictionary<string, List<GameObject>>();
         Init();
+        g_list = new List<GameObject>();
     }
 
     private void Init()
     {
         // 添加预制路径 后期加 需要的预制体+路径
         m_PrefabPathDict.Add("Cube", "Prefabs/Cube");
+        m_PrefabPathDict.Add("Bullet_Blue", "Prefabs/Bullet/Bullet_Blue");
+        m_PrefabPathDict.Add("Bullet_Explosion_Blue", "Prefabs/BulletExplosion/Bullet_Explosion_Blue");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject go = GetGameObject("Bullet_Blue");
+            go.transform.parent = null;
+            go.transform.position = transform.position;
+            go.transform.forward = transform.forward;
+            g_list.Add(go);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (g_list.Count > 0)
+            {
+                RecycleGameObject("Bullet_Blue", g_list[0]);
+                g_list.RemoveAt(0);
+            }
+        }
     }
 
     public GameObject GetGameObject(string key)
@@ -48,7 +68,7 @@ public class ObjectPoolManager : MonoBehaviour
         List<GameObject> list;
         if (m_PrefabDict.TryGetValue(key, out list))
         {
-            if(list.Count > 0)
+            if (list.Count > 0)
             {
                 GameObject go = list[0];
                 go.SetActive(true);
@@ -87,5 +107,16 @@ public class ObjectPoolManager : MonoBehaviour
             m_PrefabDict.Add(key, list);
         }
         go.transform.parent = this.transform;
+    }
+
+    public void RecycleGameObject(string key, GameObject go, float time)
+    {
+        StartCoroutine(WaitToRecycle(key, go, time));
+    }
+
+    public IEnumerator WaitToRecycle(string key, GameObject go,float time)
+    {
+        yield return new WaitForSeconds(time);
+        RecycleGameObject(key, go);
     }
 }
