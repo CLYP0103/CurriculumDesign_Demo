a4e32769690274a53f36506ad35d2af50a9950dc
RECNEPS-KIPS <2432719656@qq.com>
2019-10-15 14:16:17 +0800

    森林AI放置完成

--- Demo/Assets/Script/Controller/AIController.cs
+++ Demo/Assets/Script/Controller/AIController.cs
@@ -13,7 +13,6 @@ public class AIController : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
     }
-
     // Update is called once per frame
     private float dis = 0;
     void Update() {
