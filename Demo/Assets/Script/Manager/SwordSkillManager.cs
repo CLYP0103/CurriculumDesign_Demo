9016e184dfbc43940ea9fb72ba014a14f5cd9d05
RECNEPS-KIPS <2432719656@qq.com>
2019-10-19 22:08:54 +0800

    人物UI和开始界面

---
+++ Demo/Assets/Script/Manager/SwordSkillManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwordSkillManager : MonoBehaviour {
+    public float coldTime = 2;
+    private float timer = 0;
+    private Image cover;
+    public KeyCode key;
+    private bool isStart = false;
+    // Start is called before the first frame update
+    void Start() {
+        cover = transform.Find("cover").GetComponent<Image>();
+    }
+
+    // Update is called once per frame 
+    void Update() {
+        if (Input.GetKeyDown(key)) {
+            isStart = true;
+        }
+        if (isStart) {
+            timer += Time.deltaTime;
+            cover.fillAmount = (coldTime - timer) / coldTime;
+            if (timer >= coldTime) {
+                cover.fillAmount = 0;
+                timer = 0;
+                isStart = false;
+            }
+        }
+    }
+
+    public void OnClick() {
+        isStart = true;
+
+    }
+}
